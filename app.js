caaeee1b01fe29fe31fe028071b45d024d027ef5
Sylvia <sylviad1221@gmail.com>
2016-10-04 22:43:39 -0400

    use api to access json movie file

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var request = require("request");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view-engine", "ejs");
+
+app.get("/results", function(req, res){
+    request("http://www.omdbapi.com/?s=NewYork", function(error, response, body){
+        if(!error && response.statusCode == 200) {
+          var data = JSON.parse(body);
+          res.render("results");
+        }
+    });
+});
+
+app.listen(3000, function(){
+  console.log("Check it OUT!!!");
+});
